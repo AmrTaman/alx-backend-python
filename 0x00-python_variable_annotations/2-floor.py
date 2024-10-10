cdd921df5da24b6bfa4f2671ef9e0d9c9b23d790
AmrTaman <amrtaman4325@gmail.com>
2024-10-10 16:16:01 +0000

    we are flooring here

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/bin/usr/env/ python3
+"""
+we are flooring it here
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    operates a floor function on float
+    """
+    return math.floor(n)
