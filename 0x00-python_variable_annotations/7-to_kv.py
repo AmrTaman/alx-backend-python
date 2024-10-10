b5f7767c78beb38eff00a8cf18bb29d58edc7751
AmrTaman <amrtaman4325@gmail.com>
2024-10-10 17:21:04 +0000

    dfsd

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+asdasd
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+    """
+    iam here
+    """
+    return (k, v*v)
