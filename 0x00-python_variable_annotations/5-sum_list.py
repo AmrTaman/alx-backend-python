f778801a9a5c4e89db0de2995e40aa60bc2e6556
AmrTaman <amrtaman4325@gmail.com>
2024-10-10 16:57:49 +0000

    we are summing floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+summing up an annotated list
+"""
+from typing import List
+
+
+def sum_list(lst: List[float]) -> float:
+    """
+    we are summing up a list here
+    """
+    return sum(lst)
