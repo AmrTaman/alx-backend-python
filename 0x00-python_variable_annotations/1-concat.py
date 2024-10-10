8424753e2cdbe662bf84d0667ad7623ebad08dc2
AmrTaman <amrtaman4325@gmail.com>
2024-10-10 16:10:39 +0000

    concatoination try

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+second try for annotation
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    this function concatonaters strings
+    """
+    return str1 + str2
